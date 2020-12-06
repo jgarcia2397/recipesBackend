2ec2c7af4ba503fbd77b88da512c8dbeba435471
Josh Garcia <josh2397garcia@gmail.com>
2020-12-05 21:34:33 -0500

    Create recipes-controller to define middleware logic for GET recipe routes

---
+++ controllers/recipes-controller.js
@@ -0,0 +1,56 @@
+const HttpError = require('../models/http-error');
+
+const dummyRecipes = [
+	{
+		id: 'r1',
+		basicDetails: {
+			recipeName: 'PB & J Sandwich',
+			prepTime: '1',
+			prepTimeUnits: 'minutes',
+			cookTime: '2',
+			cookTimeUnits: 'minutes',
+			servings: '1',
+			difficulty: 'Easy',
+		},
+		ingredients: ['2 slices of bread', 'jam', 'peanut butter'],
+		directions: [
+			'toast slices of bread',
+			'spread PB and J on toast',
+			'stick two pieces of toast together',
+		],
+		creator: 'u1',
+	},
+];
+
+const getRecipeByRecipeId = (req, res, next) => {
+	const recipeId = req.params.rid;
+	const recipe = dummyRecipes.find(r => {
+		return r.id === recipeId;
+	});
+
+	if (!recipe) {
+		return next(
+			new HttpError('Could not find a recipe for the given recipe ID.', 404)
+		);
+	}
+
+	res.json({ recipe: recipe });
+};
+
+const getRecipeByUserId = (req, res, next) => {
+	const userId = req.params.uid;
+	const recipe = dummyRecipes.find(r => {
+		return r.creator === userId;
+	});
+
+	if (!recipe) {
+		return next(
+			new HttpError('Could not find a recipe for the given user ID.', 404)
+		);
+	}
+
+	res.json({ recipe: recipe });
+};
+
+exports.getRecipeByRecipeId = getRecipeByRecipeId;
+exports.getRecipeByUserId = getRecipeByUserId;
--- routes/recipes-routes.js
+++ routes/recipes-routes.js
@@ -1,59 +1,11 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
+const recipesControllers = require('../controllers/recipes-controller');
 
 const router = express.Router();
 
-const dummyRecipes = [
-	{
-		id: 'r1',
-		basicDetails: {
-			recipeName: 'PB & J Sandwich',
-			prepTime: '1',
-			prepTimeUnits: 'minutes',
-			cookTime: '2',
-			cookTimeUnits: 'minutes',
-			servings: '1',
-			difficulty: 'Easy',
-		},
-		ingredients: ['2 slices of bread', 'jam', 'peanut butter'],
-		directions: [
-			'toast slices of bread',
-			'spread PB and J on toast',
-			'stick two pieces of toast together',
-		],
-		creator: 'u1',
-	},
-];
+router.get('/:rid', recipesControllers.getRecipeByRecipeId);
 
-router.get('/:rid', (req, res, next) => {
-	const recipeId = req.params.rid;
-	const recipe = dummyRecipes.find(r => {
-		return r.id === recipeId;
-	});
-
-	if (!recipe) {
-		return next(
-			new HttpError('Could not find a recipe for the given recipe ID.', 404)
-		);
-	}
-
-	res.json({ recipe: recipe });
-});
-
-router.get('/user/:uid', (req, res, next) => {
-	const userId = req.params.uid;
-	const recipe = dummyRecipes.find(r => {
-		return r.creator === userId;
-	});
-
-	if (!recipe) {
-		return next(
-			new HttpError('Could not find a recipe for the given user ID.', 404)
-		);
-	}
-
-	res.json({ recipe: recipe });
-});
+router.get('/user/:uid', recipesControllers.getRecipeByUserId);
 
 module.exports = router;
