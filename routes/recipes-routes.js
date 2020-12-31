d2ecb6a5b729ebf8ebeb51014d380c30dc03f8e1
Josh Garcia <josh2397garcia@gmail.com>
2020-12-31 16:25:24 -0500

    Configure custom image upload middleware using Multer, use middleware in createRecipe

--- routes/recipes-routes.js
+++ routes/recipes-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const recipesControllers = require('../controllers/recipes-controller');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
@@ -11,6 +12,7 @@ router.get('/user/:uid', recipesControllers.getAllRecipesByUserId);
 
 router.post(
 	'/',
+	fileUpload.single('image'),
 	[
 		check('basicDetails.recipeName').not().isEmpty(),
 		check('basicDetails.prepTime').isNumeric(),
