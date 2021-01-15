7ed2f47eaadb17c23da598604cc07c6503d44ae6
Josh Garcia <josh2397garcia@gmail.com>
2021-01-15 18:22:57 -0500

    Create new middleware to verify token and protect create/update/delete recipe routes

--- routes/recipes-routes.js
+++ routes/recipes-routes.js
@@ -3,6 +3,7 @@ const { check } = require('express-validator');
 
 const recipesControllers = require('../controllers/recipes-controller');
 const fileUpload = require('../middleware/file-upload');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
@@ -10,17 +11,12 @@ router.get('/:rid', recipesControllers.getRecipeByRecipeId);
 
 router.get('/user/:uid', recipesControllers.getAllRecipesByUserId);
 
+router.use(checkAuth);		// Route protection for all below routes
+
 router.post(
 	'/',
 	fileUpload.single('image'),
 	[
-		// check('basicDetails.recipeName').not().isEmpty(),
-		// check('basicDetails.prepTime').isNumeric(),
-		// check('basicDetails.prepTimeUnits').not().isEmpty(),
-		// check('basicDetails.cookTime').isNumeric(),
-		// check('basicDetails.cookTimeUnits').not().isEmpty(),
-		// check('basicDetails.servings').isNumeric(),
-		// check('basicDetails.difficulty').not().isEmpty(),
 		check('recipeName').not().isEmpty(),
 		check('prepTime').isNumeric(),
 		check('prepTimeUnits').not().isEmpty(),
